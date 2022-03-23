167f83b3d31b8fc580468f976aba64dd3f2751c7
Soroush Semerkant <ssemerkant@cs.stonybrook.edu>
2022-03-23 17:18:27 -0400

    almost done with malloc

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 #include "sfmm.h"
 
-int main(int argc, char const *argv[]) {
-    double* ptr = sf_malloc(sizeof(double));
+int main(int argc, char const *argv[])
+{
+    sf_set_magic(0x0);
 
-    *ptr = 320320320e-320;
+    // printf("allocating double of size %ld bytes\n", sizeof(double));
+    // double *ptr = sf_malloc(sizeof(double));
 
-    printf("%f\n", *ptr);
+    // *ptr = 1234.5678;
 
-    sf_free(ptr);
+    // printf("address: 0x%.8lx\n", (long int)ptr);
+    // printf("value: %f\n", *ptr);
+
+    // sf_show_heap();
+    // printf("\n----------------------\n");
+
+    // printf("allocating char[1000] of size %ld bytes\n", sizeof(char[1000]));
+    // char **ptr2 = sf_malloc(sizeof(char[1000]));
+
+    // *ptr2 = "This is a 1000 character string";
+
+    // printf("address: 0x%.8lx\n", (long int)ptr2);
+    // printf("value: %s\n", *ptr2);
+
+    // // sf_show_blocks();
+    // sf_show_heap();
+    // printf("\n----------------------\n");
+
+    // printf("allocating char[940] of size %ld bytes\n", sizeof(char[940]));
+    // char **ptr3 = sf_malloc(sizeof(char[940]));
+
+    // *ptr3 = "This is a 940 character string";
+
+    // printf("address: 0x%.8lx\n", (long int)ptr3);
+    // printf("value: %s\n", *ptr3);
+
+    // // sf_show_blocks();
+    // sf_show_heap();
+    // printf("\n----------------------\n");
+    // sf_free(ptcd
+
+    // printf("allocating char[98304] of size %ld bytes\n", sizeof(char[98304]));
+    // char **ptr4 = sf_malloc(sizeof(char[98304]));
+    // if (!ptr4)
+    //     return EXIT_FAILURE;
+    // *ptr4 = "This is should fail";
+
+    // printf("address: 0x%.8lx\n", (long int)ptr4);
+    // printf("value: %s\n", *ptr4);
+
+    // // sf_show_blocks();
+    // sf_show_heap();
+    // printf("\n----------------------\n");
+
+    printf("allocating char[4032] of size %ld bytes\n", sizeof(char[4032]));
+    char **ptr5 = sf_malloc(sizeof(char[4032]));
+    if (!ptr5)
+        return EXIT_FAILURE;
+    *ptr5 = "This is should get 4 pages";
+
+    printf("address: 0x%.8lx\n", (long int)ptr5);
+    printf("value: %s\n", *ptr5);
+
+    // sf_show_blocks();
+    sf_show_heap();
+    printf("\n----------------------\n");
+    // sf_free(ptcd
 
     return EXIT_SUCCESS;
 }
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include "debug.h"
 #include "sfmm.h"
+#include "errno.h"
 
 /*
  ---------------------------------------------PERSONAL MACROS----------------------------------------------------
@@ -17,13 +18,11 @@
 
 #define BLOCK_BYTES 0x00000000FFFFFFF0
 
-#define SET_PAYLOAD_BYTES 0x000000000FFFFFFFF
+#define SET_PAYLOAD_BYTES 0x00000000FFFFFFFF
 #define SET_BLOCK_BYTES 0xFFFFFFFF0000000F
 #define SET_ALLOC_BYTES 0xFFFFFFFFFFFFFFFB
 #define SET_PRV_ALLOC_BYTES 0xFFFFFFFFFFFFFFFD
 #define SET_IN_QCKLST_BYTES 0xFFFFFFFFFFFFFFFE
-
-#define ENOMEM -1
 
 /*
  ---------------------------------------GLOBAL VARIABLES----------------------------------------------------
@@ -34,45 +33,64 @@ sf_block *epilogue;
 /*
  ---------------------------------------GETTER/SETTER FUNCTIONS----------------------------------------------------
 */
+sf_header obfiscate(sf_header *header)
+{
+    return ((long int)*header) ^ MAGIC;
+}
+
 void set_block_payload_size(sf_header *header, sf_header size)
 {
     size <<= 32;
+    *header = obfiscate(header);
     *header = (*header & SET_PAYLOAD_BYTES) + size;
+    *header = obfiscate(header);
 }
 
 int get_block_size(sf_header *header)
 {
-    return (*header & BLOCK_BYTES) >> 4;
+    sf_header unobfiscated_header = obfiscate(header);
+    return (unobfiscated_header & BLOCK_BYTES);
 }
+
 void set_block_size(sf_header *header, int new_size)
 {
-    new_size = new_size << 4;
-    *header = (*header & SET_BLOCK_BYTES) + new_size;
+    // new_size <<= ;
+    sf_header unobfiscated_header = obfiscate(header);
+    *header = (unobfiscated_header & SET_BLOCK_BYTES) + new_size;
+    *header = obfiscate(header);
 }
 
 int get_header_alloc(sf_header *header)
 {
-    return (*header & THIS_BLOCK_ALLOCATED) >> 2;
+    sf_header unobfiscated_header = obfiscate(header);
+    return (unobfiscated_header & THIS_BLOCK_ALLOCATED) >> 2;
 }
 void set_block_alloc(sf_header *header, int alloc)
 {
     alloc <<= 2;
+    *header = obfiscate(header);
     *header = (*header & SET_ALLOC_BYTES) + alloc;
+    *header = obfiscate(header);
 }
 
 int get_header_prv_alloc(sf_header *header)
 {
-    return (*header & PREV_BLOCK_ALLOCATED) >> 1;
+    sf_header unobfiscated_header = obfiscate(header);
+    return (unobfiscated_header & PREV_BLOCK_ALLOCATED) >> 1;
 }
 void set_block_prv_alloc(sf_header *header, int prv_alloc)
 {
     prv_alloc <<= 1;
+    *header = obfiscate(header);
     *header = (*header & SET_PRV_ALLOC_BYTES) + prv_alloc;
+    *header = obfiscate(header);
 }
 
 void set_block_in_qcklst(sf_header *header, int in_qcklst)
 {
+    *header = obfiscate(header);
     *header = (*header & SET_IN_QCKLST_BYTES) + in_qcklst;
+    *header = obfiscate(header);
 }
 
 /*
@@ -147,73 +165,9 @@ int get_free_list_index(sf_size_t size)
 
 sf_block *get_prev_heap_block(sf_block *block)
 {
-    sf_footer *prev_footer = &block->prev_footer;
-    sf_block *prev_block = (sf_block *)(prev_footer - (get_block_size(prev_footer) * 8) + (HEADER_SIZE * 8));
+    // sf_block *prev_block = (sf_block *)(prev_footer - (get_block_size(prev_footer) * 8) + (HEADER_SIZE * 8));
+    sf_block *prev_block = (sf_block *)((long int)block - (get_block_size(&block->prev_footer)));
     return prev_block;
-}
-
-sf_block *coalesce(sf_block *block)
-{
-    if (!get_header_prv_alloc(&block->header))
-    {
-        sf_block *prev_block = get_prev_heap_block(block);
-        int prev_block_size = get_block_size(&prev_block->header);
-        int curr_block_size = get_block_size(&block->header);
-        set_block_size(&prev_block->header, prev_block_size + curr_block_size);
-        return prev_block;
-    }
-    else
-        return block;
-    // TO BE IMPLEMENTED
-    // gotta coalesce the next block
-}
-
-sf_block *mem_grow_block()
-{
-    sf_block *new_page = epilogue;
-    void *mem_grow = sf_mem_grow();
-    if (!mem_grow)
-        return NULL;
-    epilogue = memcpy((void *)(sf_mem_end() - 64), epilogue, 8);
-    set_block_alloc(&new_page->header, 0);
-    set_block_in_qcklst(&new_page->header, 0);
-    set_block_prv_alloc(&new_page->header, 0); // fix
-    set_block_size(&new_page->header, PAGE_SZ);
-    return new_page;
-}
-
-void *check_free_lists(sf_size_t size)
-{
-
-    for (int free_list_index = get_free_list_index(size); free_list_index < NUM_FREE_LISTS; free_list_index++)
-    {
-        sf_block list_head = sf_free_list_heads[free_list_index];
-        sf_block *current_block = list_head.body.links.next;
-        if (!list_head.body.links.next)
-            continue;
-        while (&list_head != current_block)
-        {
-            if (get_block_size(&current_block->header) >= size)
-            {
-                sf_block *prev_block = current_block->body.links.prev;
-                sf_block *next_block = current_block->body.links.next;
-                prev_block->body.links.next = next_block;
-                next_block->body.links.prev = prev_block;
-                return current_block;
-            }
-            current_block = current_block->body.links.next;
-        }
-    }
-    sf_block *coalesced_block;
-    while (size > 0)
-    {
-        sf_block *new_block = mem_grow_block();
-        if (!new_block)
-            return NULL;
-        coalesced_block = coalesce(new_block);
-        size -= PAGE_SZ + get_block_size(&coalesced_block->header);
-    }
-    return coalesced_block;
 }
 
 void place_in_free_list(sf_block *block)
@@ -237,17 +191,109 @@ void place_in_free_list(sf_block *block)
     sentinell->body.links.next = block;
 }
 
+void remove_from_free_list(sf_block *block)
+{
+    if (!block->body.links.next || !block->body.links.prev)
+        return;
+    sf_block *prev_block = block->body.links.prev;
+    sf_block *next_block = block->body.links.next;
+    prev_block->body.links.next = next_block;
+    next_block->body.links.prev = prev_block;
+    block->body.links.next = NULL;
+    block->body.links.prev = NULL;
+}
+
+sf_block *coalesce(sf_block *block)
+{
+    if (!get_header_prv_alloc(&block->header))
+    {
+        if (block->body.links.next && block->body.links.prev)
+            remove_from_free_list(block);
+        sf_block *prev_block = get_prev_heap_block(block);
+        remove_from_free_list(prev_block);
+        int prev_block_size = get_block_size(&prev_block->header);
+        int curr_block_size = get_block_size(&block->header);
+        set_block_size(&prev_block->header, prev_block_size + curr_block_size);
+        place_in_free_list(prev_block);
+        return prev_block;
+    }
+    else
+        return block;
+    // TO BE IMPLEMENTED
+    // gotta coalesce the next block
+}
+
+sf_block *mem_grow_block()
+{
+    sf_block *new_page = epilogue;
+    void *mem_grow = sf_mem_grow();
+    if (!mem_grow)
+        return NULL;
+    size_t epilogue_size = sizeof(*epilogue);
+    epilogue = memcpy((void *)(sf_mem_end() - HEADER_SIZE - HEADER_SIZE), epilogue, epilogue_size);
+    set_block_alloc(&new_page->header, 0);
+    set_block_in_qcklst(&new_page->header, 0);
+    set_block_prv_alloc(&new_page->header, 0); // fix
+    set_block_size(&new_page->header, PAGE_SZ);
+
+    new_page->prev_footer = *((sf_footer *)((long int)new_page));
+    return new_page;
+}
+
+void *check_free_lists(sf_size_t size)
+{
+
+    for (int free_list_index = get_free_list_index(size); free_list_index < NUM_FREE_LISTS; free_list_index++)
+    {
+        sf_block *list_head = &sf_free_list_heads[free_list_index];
+        sf_block *current_block = list_head->body.links.next;
+        if (!list_head->body.links.next)
+            continue;
+        while (list_head != current_block)
+        {
+            if (get_block_size(&current_block->header) >= size)
+            {
+                sf_block *prev_block = current_block->body.links.prev;
+                sf_block *next_block = current_block->body.links.next;
+                prev_block->body.links.next = next_block;
+                next_block->body.links.prev = prev_block;
+                return current_block;
+            }
+            current_block = current_block->body.links.next;
+        }
+    }
+    sf_block *coalesced_block;
+    int integer_size = size;
+    while (integer_size >= 0)
+    {
+        sf_block *new_block = mem_grow_block();
+        // sf_show_heap();
+        if (!new_block)
+            return NULL;
+        coalesced_block = coalesce(new_block);
+        integer_size -= get_block_size(&coalesced_block->header);
+    }
+    return coalesced_block;
+}
+
 void split(sf_block *block, sf_size_t size)
 {
     int curr_size = get_block_size(&block->header);
     set_block_size(&block->header, size);
-    sf_block *new_block = (sf_block *)((long int)block + (size * 8));
+    remove_from_free_list(block);
+    sf_block *new_block = (sf_block *)((long int)block + (size));
     set_block_size(&new_block->header, curr_size - size);
     set_block_alloc(&new_block->header, 0);
     set_block_prv_alloc(&new_block->header, 1);
     set_block_in_qcklst(&new_block->header, 0);
+    sf_footer *footer = (sf_footer *)((long int)block + curr_size);
+    *footer = new_block->header;
     place_in_free_list(new_block);
     new_block->prev_footer = block->header;
+    if ((sf_block *)((long int)new_block + get_block_size(&new_block->header)) == epilogue)
+    {
+        set_block_prv_alloc(&epilogue->header, 0);
+    }
 }
 
 void allocate(sf_block *block)
@@ -272,7 +318,7 @@ void initialize_free_lists()
 {
     for (int i = 0; i < NUM_FREE_LISTS; i++)
     {
-        if (!&sf_free_list_heads[i])
+        if (!sf_free_list_heads[i].body.links.next || !sf_free_list_heads[i].body.links.prev)
         {
             sf_free_list_heads[i].body.links.next = &sf_free_list_heads[i];
             sf_free_list_heads[i].body.links.prev = &sf_free_list_heads[i];
@@ -299,29 +345,34 @@ void *sf_malloc(sf_size_t size)
             sf_errno = ENOMEM;
             return NULL;
         }
-        sf_block *prologue = (sf_block *)(mem_grow + 64);
+        sf_block *prologue = (sf_block *)(mem_grow);
         set_block_payload_size(&prologue->header, 0);
         set_block_size(&prologue->header, MIN_BLOCK_SIZE);
         set_block_alloc(&prologue->header, 1);
         set_block_in_qcklst(&prologue->header, 0);
 
-        sf_block *first_block = (sf_block *)(prologue + MIN_BLOCK_SIZE * 8);
-        set_block_size(&first_block->header, PAGE_SZ - MIN_BLOCK_SIZE - HEADER_SIZE - HEADER_SIZE);
+        sf_block *first_block = (sf_block *)(sf_mem_start() + 4 * HEADER_SIZE);
+        set_block_size(&first_block->header, PAGE_SZ - MIN_BLOCK_SIZE - 2 * HEADER_SIZE);
         set_block_alloc(&first_block->header, 0);
         set_block_prv_alloc(&first_block->header, 1);
         set_block_in_qcklst(&first_block->header, 0);
+        place_in_free_list(first_block);
 
-        epilogue = (sf_block *)(first_block + (PAGE_SZ - MIN_BLOCK_SIZE - HEADER_SIZE - HEADER_SIZE));
+        epilogue = (sf_block *)(sf_mem_end() - HEADER_SIZE - HEADER_SIZE);
         set_block_payload_size(&prologue->header, 0);
         set_block_size(&epilogue->header, 0);
         set_block_alloc(&epilogue->header, 1);
         set_block_prv_alloc(&epilogue->header, 0);
         set_block_in_qcklst(&epilogue->header, 0);
+        // epilogue->header = first_block->header;
 
-        place_in_free_list(first_block);
+        sf_footer *footer = (sf_footer *)((long int)epilogue);
+        *footer = first_block->header;
+
+        // sf_show_heap();
+
         first_alloc = 0;
     }
-    // sf_show_blocks();
     sf_block *address = check_quick_lists(size);
     if (address)
         goto block_found;
@@ -338,8 +389,12 @@ block_found:
         split(address, size);
     }
     allocate(address);
+    if ((sf_block *)((long int)address + get_block_size(&address->header)) == epilogue)
+    {
+        set_block_prv_alloc(&epilogue->header, 1);
+    }
     set_block_payload_size(&address->header, client_size);
-    return address + 64;
+    return &address->body.payload;
 }
 
 void sf_free(void *pp)
