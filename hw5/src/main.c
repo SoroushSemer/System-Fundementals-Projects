17c1615bf6342abaf28b5a036f43ba47ae895cc7
Soroush Semerkant <ssemerkant@cs.stonybrook.edu>
2022-04-28 12:10:18 -0400

    first commit with work

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
 #include "pbx.h"
 #include "server.h"
@@ -7,15 +12,40 @@
 
 static void terminate(int status);
 
+int get_port(int argc, char *argv[])
+{
+    int c, port;
+    while ((c = getopt(argc, argv, "p:")) != -1)
+    {
+        switch (c)
+        {
+        case 'p':
+            port = atoi(optarg);
+            break;
+
+        default:
+            debug("get_opt failed");
+            return -1;
+            break;
+        }
+    }
+    return port;
+}
+
 /*
  * "PBX" telephone exchange simulation.
  *
  * Usage: pbx <port>
  */
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+
+    int port = get_port(argc, argv);
+    if (port < 0)
+        return -1;
 
     // Perform required initialization of the PBX module.
     debug("Initializing PBX...");
@@ -27,8 +57,75 @@ int main(int argc, char* argv[]){
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
 
+    struct sigaction sa;
+    sa.sa_handler = &terminate;
+    if (sigaction(SIGHUP, &sa, NULL) < 0)
+    {
+        debug("sigaction failed");
+        return -1;
+    }
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+    {
+        debug("socket failed");
+        return -1;
+    }
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        debug("setsockopt failed");
+        close(server_fd);
+        return -1;
+    }
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        debug("bind failed");
+        close(server_fd);
+        return -1;
+    }
+
+    if (listen(server_fd, 256) < 0)
+    {
+        debug("listen failed");
+        close(server_fd);
+        return -1;
+    }
+
+    struct sockaddr_storage server_storage;
+    int addrlen;
+    // int client[256];
+    pthread_t tid[256];
+    for (int i = 0;; i++)
+    {
+        int *client = calloc(1, sizeof(int));
+        addrlen = sizeof(server_storage);
+        debug("listening on port %d for client #%d", port, i + 1);
+        *client = accept(server_fd, (struct sockaddr *)&server_storage, (socklen_t *)&addrlen);
+        if (*client < 0)
+        {
+            debug("accept failed");
+            return -1;
+        }
+
+        debug("client #%d accepted", i + 1);
+        debug("creating thread #%d", i + 1);
+        if (pthread_create(&tid[i], NULL, &pbx_client_service, (void *)client) < 0)
+        {
+            debug("client #%d thread failed", i + 1);
+            return -1;
+        }
+        debug("client #%d thread created", i + 1);
+    }
+
     fprintf(stderr, "You have to finish implementing main() "
-	    "before the PBX server will function.\n");
+                    "before the PBX server will function.\n");
 
     terminate(EXIT_FAILURE);
 }
@@ -36,7 +133,8 @@ int main(int argc, char* argv[]){
 /*
  * Function called to cleanly shut down the server.
  */
-static void terminate(int status) {
+static void terminate(int status)
+{
     debug("Shutting down PBX...");
     pbx_shutdown(pbx);
     debug("PBX server terminating");
--- hw5/src/server.c
+++ hw5/src/server.c
@@ -3,19 +3,79 @@
  * Manages interaction with a client telephone unit (TU).
  */
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <string.h>
 #include "debug.h"
 #include "pbx.h"
 #include "server.h"
 
+#define PICK "pick"
+#define HANG "hang"
 /*
  * Thread function for the thread that handles interaction with a client TU.
  * This is called after a network connection has been made via the main server
  * thread and a new thread has been created to handle the connection.
  */
-#if 0
-void *pbx_client_service(void *arg) {
+// #if 0
+
+void *pbx_client_service(void *arg)
+{
     // TO BE IMPLEMENTED
-    abort();
+    int *client_fd = calloc(1, sizeof(int));
+    *client_fd = *((int *)arg);
+    free(arg);
+    debug("client fd freed %d", *((int *)arg));
+
+    if (pthread_detach((client_fd) < 0))
+    {
+        debug("detach failed!");
+        return NULL;
+    }
+
+    TU *client_tu = tu_init(*client_fd);
+
+    pbx_register(pbx, client_tu, *client_fd);
+
+    for (;;)
+    {
+        char buf[7];
+        read(*client_fd, buf, 4);
+        if (!strcmp(buf, tu_command_names[TU_DIAL_CMD]))
+        {
+            debug("DIAL");
+        }
+        else if (!strcmp(buf, tu_command_names[TU_CHAT_CMD]))
+        {
+            debug("CHAT");
+        }
+        else if (!strcmp(buf, PICK) || !strcmp(buf, HANG))
+        {
+            read(*client_fd, buf, 2);
+            if (!strcmp(buf, tu_command_names[TU_PICKUP_CMD]))
+            {
+                debug("PICKUP");
+            }
+            else
+            {
+                read(*client_fd, buf, 1);
+                if (!strcmp(buf, tu_command_names[TU_HANGUP_CMD]))
+                {
+                    debug("HANGUP");
+                }
+                else
+                {
+                    debug("INVALID CMD");
+                    return NULL;
+                }
+            }
+        }
+    }
+    return NULL;
+    // abort();
 }
-#endif
+// #endif
